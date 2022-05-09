bc389f432d090585261543fa627538132e82b166
Kay <kard@hotmail.co.uk>
2022-05-09 16:05:22 +0100

    Returns fizzbuzz for multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,10 @@
 def fizzbuzz(number)
-  if number % 3 == 0 
+  if number % 3 == 0 && number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0 
     return 'fizz' 
   elsif number % 5 == 0 
-   return 'buzz' 
+    return 'buzz' 
   end 
 end
 
