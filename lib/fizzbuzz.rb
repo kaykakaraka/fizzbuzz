b51d8a3dbc1beedb357f4395b1dd36385c29a8ea
Kay <kard@hotmail.co.uk>
2022-05-09 15:46:52 +0100

    Added tests for multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,8 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 
+    return 'fizz' 
+  elsif number % 5 == 0 
+   return 'buzz' 
+  end 
 end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,20 @@
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 12' do
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 20' do
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
