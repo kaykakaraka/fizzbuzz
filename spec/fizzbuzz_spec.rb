dd591a786040403ae3e9eba5f5c14cea98c09b9b
fridayshoes <one_amp@hotmail.com>
2022-05-10 11:19:00 +0000

    Test for non-multiple of 3 & 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -21,4 +21,8 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed a multiple of 3 & 5 ' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+  it 'returns number when not a multiple of 3 & 5' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
 end
